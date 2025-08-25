a81f49a3c4eea2e7c1904e964c2634f49f22efe0
Sawyer Maloney <sawyerdmaloney@gmail.com>
2025-08-24 23:14:04 -0400

    removing target from tracking

--- tcpbasics/target/debug/deps/tcpbasics-f27482b90f0bfca0.d
+++
@@ -1,5 +0,0 @@
-/mnt/c/Users/Tonio/Desktop/rustlang/TCPBasics/tcpbasics/target/debug/deps/tcpbasics-f27482b90f0bfca0.d: src/main.rs
-
-/mnt/c/Users/Tonio/Desktop/rustlang/TCPBasics/tcpbasics/target/debug/deps/tcpbasics-f27482b90f0bfca0: src/main.rs
-
-src/main.rs:
--- tcpbasics/target/debug/tcpbasics.d
+++
@@ -1,1 +0,0 @@
-/mnt/c/Users/Tonio/Desktop/rustlang/TCPBasics/tcpbasics/target/debug/tcpbasics: /mnt/c/Users/Tonio/Desktop/rustlang/TCPBasics/tcpbasics/src/main.rs
